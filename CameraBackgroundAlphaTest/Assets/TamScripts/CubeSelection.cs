f0c95215a923dfbb6b6f011c2ee1e09968a19716
Tam66662 <Tam66662@outlook.com>
2017-08-04 11:47:41 -0700

    HoloLens demo that changes the main camera's background color, thus changing the optical see-thru quality.

---
+++ CameraBackgroundAlphaTest/Assets/TamScripts/CubeSelection.cs
@@ -0,0 +1,53 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CubeSelection : MonoBehaviour, IInputClickHandler {
+
+    public TextMesh textMesh;
+
+    private float rotationAmount = 50;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (GazeManager.Instance.HitObject != null && GazeManager.Instance.HitObject.name == this.name)
+        {
+            if (this.name == "White")
+            {
+                Camera.main.backgroundColor = Color.white;
+                if (Camera.main.clearFlags == CameraClearFlags.Skybox) Camera.main.clearFlags = CameraClearFlags.SolidColor;
+            }
+            else if (this.name == "Black")
+            {
+                Camera.main.backgroundColor = Color.black;
+                if (Camera.main.clearFlags == CameraClearFlags.Skybox) Camera.main.clearFlags = CameraClearFlags.SolidColor;
+            }
+            else if (this.name == "Gray")
+            {
+                Camera.main.backgroundColor = Color.gray;
+                if (Camera.main.clearFlags == CameraClearFlags.Skybox) Camera.main.clearFlags = CameraClearFlags.SolidColor;
+            }
+            else if (this.name == "Skybox")
+            {
+                Camera.main.clearFlags = CameraClearFlags.Skybox;
+            }
+
+            textMesh.text = string.Format("Background is set to {0}", this.name);
+        }
+    }
+
+    private void Start()
+    {
+        if (this.name == "White")
+        {
+            textMesh.text = "Tap a color to change the background";
+        }
+    }
+
+    private void Update()
+    {
+        this.transform.Rotate(Vector3.up, rotationAmount * Time.deltaTime);
+    }
+}
