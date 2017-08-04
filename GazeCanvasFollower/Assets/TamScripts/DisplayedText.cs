19ec9b5903e966e9819e2034dc994760ef02e334
Tam66662 <Tam66662@outlook.com>
2017-08-04 11:30:45 -0700

    Hololens demo to have some help text displayed in the user's gaze at all times.

---
+++ GazeCanvasFollower/Assets/TamScripts/DisplayedText.cs
@@ -0,0 +1,28 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayedText : MonoBehaviour {
+
+    [Tooltip("Reference to TextMesh component where the FPS should be displayed.")]
+    [SerializeField]
+    private TextMesh textMesh;
+
+    private SphereBasedTagalong sphereBasedTagalong;
+
+    // Use this for initialization
+    void Start () {
+        sphereBasedTagalong = GetComponentInParent<SphereBasedTagalong>();
+        textMesh.text = string.Format(
+            "This text will follow your gaze, within a Sphere " + "\n" +
+            "Radius of {0} and a Move Speed of {1}. To tweak " + "\n" +
+            "these values, edit the \"Sphere Based Tagalong\" script " + "\n" +
+            "on the \"FPSDisplay\" GameObject of your scene to see " + "\n" +
+            "how those values affect the canvas's behavior.", sphereBasedTagalong.SphereRadius, sphereBasedTagalong.MoveSpeed);
+    }
+
+    private void Update()
+    {
+    }
+}
