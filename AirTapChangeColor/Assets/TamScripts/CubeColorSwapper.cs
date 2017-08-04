3418e90ff2c156f88d4bd14b3290634e516d6655
Tam66662 <Tam66662@outlook.com>
2017-08-04 11:24:21 -0700

    HoloLens air tap demo

---
+++ AirTapChangeColor/Assets/TamScripts/CubeColorSwapper.cs
@@ -0,0 +1,21 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CubeColorSwapper : MonoBehaviour, IInputClickHandler {
+
+    public Material first;
+
+    public Material second;
+
+    private bool toggleColor;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        var meshRenderer = GetComponent<Renderer>();
+        meshRenderer.material = toggleColor ? first : second;
+        toggleColor = !toggleColor;
+    }
+}
