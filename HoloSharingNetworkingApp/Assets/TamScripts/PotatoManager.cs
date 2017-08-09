8b374c02bdddf7465dfa65efff86d263c09f5394
Tam66662 <Tam66662@outlook.com>
2017-08-08 17:43:56 -0700

    HoloLens app that showcases a sharing service, network connections, passing info to other users, and world anchors

---
+++ HoloSharingNetworkingApp/Assets/TamScripts/PotatoManager.cs
@@ -0,0 +1,246 @@
+﻿using HoloToolkit.Sharing;
+using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.XR.WSA;
+using UnityEngine.XR.WSA.Persistence;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using HoloToolkit.Unity;
+
+public class PotatoManager : Singleton<PotatoManager>, IInputClickHandler {
+
+    public static bool HasPotato { get; set; }
+
+    public static bool Placing { get; set; }
+
+    public static bool HasWorldAnchor { get; set; }
+
+    private float rotationAmount = 50f;
+
+    public TextMesh textMesh;
+
+    private WorldAnchorStore worldAnchorStore;
+
+    private MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+#if !UNITY_EDITOR
+        WorldAnchorStore.GetAsync(LoadWorldAnchorStore);
+#else
+        LogMe("SKIPPING: UNITY_EDITOR cannot load the world anchor store");
+#endif
+        meshRenderer = this.gameObject.GetComponent<MeshRenderer>();
+    }
+
+    private void LoadWorldAnchorStore(WorldAnchorStore store)
+    {
+#if !UNITY_EDITOR
+        LogMe("LoadWorldAnchorStore");
+
+        this.worldAnchorStore = store;
+        if (store.anchorCount > 0)
+        {
+            // If you have an anchor, place the potato at this anchor point.
+            var allIds = store.GetAllIds();
+            var potatoId = allIds.FirstOrDefault(id => id == this.gameObject.name);
+            if (potatoId != null)
+            {
+                // Load the store's anchor back into this potato
+                var anchor = store.Load(potatoId, this.gameObject);
+                LogMe("\tStored anchor found");
+            }
+            else
+            {
+                LogMe(string.Format("\tNo anchor '{0} as found", this.gameObject.name));
+            }
+        }
+        else
+        {
+            LogMe(string.Format("\tNo anchors exist"));
+        }
+#endif
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // If you have the potato, then it should be placed at the spot of your gaze.
+        if (Placing)
+        {
+            this.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 1;
+            this.transform.Rotate(Vector3.up, rotationAmount * Time.deltaTime);
+        }
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (HasPotato)
+        {
+            // Prior to changing the state of Placing, remove or set the world anchor.  This way,
+            // it doesn't crash because Placing affects the Update() method that runs 60 fps.
+            if (!Placing)
+            {
+                // Release the world anchor and then broadcast it to other users.
+                RemoveWorldAnchor();
+            }
+            else
+            {
+                // Set the world anchor and then broadcast it to other users.
+                SetWorldAnchor();
+            }
+
+            Placing = !Placing;
+
+            // Send a message that you are moving the object
+            if (Placing)
+            {
+                SharingStatus.Instance.SendNetworkUserMessage(SharingStatus.UserMessageId.ObjectPickedUp);
+            }
+            else
+            {
+                SharingStatus.Instance.SendNetworkUserMessage(SharingStatus.UserMessageId.ObjectPlaced, this.gameObject.transform);
+
+                // Once you place the potato, you no longer have control of it.  The other user is now given control.
+                // Unless, you are the only user in the game.
+                if (SharingStage.Instance.SessionUsersTracker.CurrentUsers.Count > 1)
+                {
+                    HasPotato = false;
+                }
+            }
+
+            UpdatePotatoColor();
+
+            LogMe(string.Format("You {0} the potato", HasPotato && Placing ? "picked up" : "dropped"));
+        }
+        else
+        {
+            LogMe("You can't do that because you do not have the potato");
+        }
+
+    }
+
+    public void UpdatePotatoColor()
+    {
+        if (meshRenderer.material != null)
+        {
+            if (HasPotato)
+            {
+                if (Placing)
+                {
+                    meshRenderer.material.color = Color.red;
+                }
+                else
+                {
+                    meshRenderer.material.color = Color.yellow;
+                }
+            }
+            else
+            {
+                if (Placing)
+                {
+                    meshRenderer.material.color = Color.yellow;
+                }
+                else
+                {
+                    meshRenderer.material.color = Color.green;
+                }
+            }
+        }
+    }
+
+    private void Anchor_OnTrackingChanged(WorldAnchor self, bool located)
+    {
+        if (located)
+        {
+            worldAnchorStore.Save(this.gameObject.name, self);
+            LogMe(string.Format("'{0}' anchor has been saved.", this.gameObject.name));
+        }
+    }
+
+    internal void SetWorldAnchor()
+    {
+#if !UNITY_EDITOR
+        // Set the world anchor and then broadcast it to other users.
+        var anchor = this.gameObject.AddComponent<WorldAnchor>();
+        anchor.OnTrackingChanged += this.Anchor_OnTrackingChanged;
+#else
+        LogMe("SKIPPING: UNITY_EDITOR cannot set the world anchor");
+#endif
+    }
+
+    internal void RemoveWorldAnchor()
+    {
+#if !UNITY_EDITOR
+        var worldAnchor = this.gameObject.GetComponent<WorldAnchor>();
+        if (worldAnchor != null)
+        {
+            DestroyImmediate(worldAnchor);
+        }
+
+        if (worldAnchorStore.Delete(this.gameObject.name))
+        {
+            LogMe(string.Format("'{0}' anchor has been deleted.", this.gameObject.name));
+        }
+#else
+        LogMe("SKIPPING: UNITY_EDITOR cannot remove the world anchor");
+#endif
+    }
+
+    private void LogMe(string message)
+    {
+        if (textMesh != null)
+        {
+            textMesh.text += string.Format("{0}\n", message);
+        }
+    }
+
+    public void PotatoInitialize()
+    {
+        if (SharingStage.Instance.SessionUsersTracker != null)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var user in SharingStage.Instance.SessionUsersTracker.CurrentUsers)
+            {
+                var xName = user.GetName();
+                if (stringBuilder.ToString() == string.Empty)
+                {
+                    stringBuilder.Append(string.Format("\tUsers: {0}", xName.ToString()));
+                }
+                else
+                {
+                    stringBuilder.Append(", " + xName.ToString());
+                }
+            }
+
+            textMesh.text += string.Format("{0} (Count Now: {1})\n", stringBuilder.ToString(), SharingStage.Instance.SessionUsersTracker.CurrentUsers.Count);
+        }
+
+        if (SharingStatus.FirstUser)
+        {
+            // If the cube is in the original world position, move it to be in front of the user's gaze.
+            if (this.gameObject.transform.position == Vector3.zero)
+            {
+                this.gameObject.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 1;
+            }
+
+            HasPotato = true;
+            LogMe("You have the hot potato");
+        }
+        else
+        {
+            // If the cube is in the original world position, move it to be in front of the user's gaze.
+            if (this.gameObject.transform.position == Vector3.zero)
+            {
+                this.gameObject.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 1;
+            }
+
+            LogMe("You do not have the hot potato");
+        }
+
+        UpdatePotatoColor();
+    }
+}
---
+++ HoloSharingNetworkingApp/Assets/TamScripts/SharingStatus.cs
@@ -0,0 +1,264 @@
+﻿using HoloToolkit.Sharing;
+using HoloToolkit.Unity;
+using System;
+using UnityEngine;
+
+public class SharingStatus : Singleton<SharingStatus> {
+
+    public TextMesh textMesh;
+
+    public static bool FirstUser { get; set; }
+
+    public enum UserMessageId : byte
+    {
+        ObjectPickedUp = MessageID.UserMessageIDStart,
+        ObjectPlaced,
+    }
+
+    /// <summary>
+    /// Used to route incoming messages from the network back to methods defined in this class.
+    /// </summary>
+    NetworkConnectionAdapter adapter;
+
+    /// <summary>
+    /// A cache of the network connection used by the sharing service.
+    /// </summary>
+    NetworkConnection connection;
+
+    /// <summary>
+    /// An identifier of the current user on the active server connection
+    /// </summary>
+    int localUserId;
+
+    // Use this for initialization
+    void Start () {
+        LogMe(string.Format("{0}: Waiting for server connection", "Start"));
+
+        if (Camera.main != null)
+        {
+            if (UnityEngine.XR.WSA.HolographicSettings.IsDisplayOpaque)
+            {
+                Camera.main.clearFlags = CameraClearFlags.Skybox;
+            }
+            else
+            {
+                Camera.main.clearFlags = CameraClearFlags.SolidColor;
+            }
+        }
+
+        if (SharingStage.Instance != null)
+        {
+            SharingStage.Instance.SharingManagerConnected += this.Instance_SharingManagerConnected;
+            SharingStage.Instance.SessionsTracker.CurrentUserJoined += this.SessionsTracker_CurrentUserJoined;
+            SharingStage.Instance.SessionsTracker.CurrentUserLeft += this.SessionsTracker_CurrentUserLeft;
+            SharingStage.Instance.SessionsTracker.ServerConnected += this.SessionsTracker_ServerConnected;
+            SharingStage.Instance.SessionsTracker.ServerDisconnected += this.SessionsTracker_ServerDisconnected;
+            SharingStage.Instance.SessionsTracker.SessionAdded += this.SessionsTracker_SessionAdded;
+            SharingStage.Instance.SessionsTracker.SessionClosed += this.SessionsTracker_SessionClosed;
+            SharingStage.Instance.SessionsTracker.SessionCreated += this.SessionsTracker_SessionCreated;
+            SharingStage.Instance.SessionsTracker.UserChanged += this.SessionsTracker_UserChanged;
+            SharingStage.Instance.SessionsTracker.UserJoined += this.SessionsTracker_UserJoined;
+            SharingStage.Instance.SessionsTracker.UserLeft += this.SessionsTracker_UserLeft;
+            SharingStage.Instance.SessionUsersTracker.UserJoined += this.SessionUsersTracker_UserJoined;
+            SharingStage.Instance.SessionUsersTracker.UserLeft += this.SessionUsersTracker_UserLeft;
+
+            // Get the current sharing service's network connection
+            connection = SharingStage.Instance.Manager.GetServerConnection();
+            LogMe("Network connection is now cached");
+
+            // Create a new network adapter so that you can route messages to and from this active user.
+            adapter = new NetworkConnectionAdapter();
+
+            // Register to the MessageReceived event
+            adapter.MessageReceivedCallback += this.Adapter_MessageReceivedCallback;
+
+            // Cache this user's id from the network
+            this.localUserId = SharingStage.Instance.Manager.GetLocalUser().GetID();
+
+            // Loop through each user message id that you plan to support, and listen for its id.
+            foreach (var supportedMessageId in Enum.GetValues(typeof(UserMessageId)))
+            {
+                LogMe(string.Format("Registering: 0x{0:X4}", (byte)supportedMessageId));
+                connection.AddListener((byte)supportedMessageId, adapter);
+            }
+        }
+        else
+        {
+            Debug.Log("ruh roh!");
+        }
+    }
+
+    private void Adapter_MessageReceivedCallback(NetworkConnection connection, NetworkInMessage msg)
+    {
+        byte messageType = msg.ReadByte();
+        var userId = msg.ReadInt32();
+
+        // Do something here when you receive the message
+        LogMe(string.Format("Message Received: 0x{0}", messageType.ToString("X2")));
+        var enumValue = (UserMessageId)messageType;
+        if (enumValue == UserMessageId.ObjectPickedUp)
+        {
+            LogMe(string.Format("Message: User {0} picked up the object", userId));
+            PotatoManager.Placing = true;
+        }
+        else if (enumValue == UserMessageId.ObjectPlaced)
+        {
+            LogMe(string.Format("Message: User {0} placed the object", userId));
+
+            // If the object was placed, then the position and rotation of the object was sent as well.  Retrieve that data.
+            var vector = new Vector3(msg.ReadFloat(), msg.ReadFloat(), msg.ReadFloat());
+            var rotation = new Quaternion(msg.ReadFloat(), msg.ReadFloat(), msg.ReadFloat(), msg.ReadFloat());
+            LogMe(string.Format("Vector: {0},{1},{2}", vector.x, vector.y, vector.z));
+            LogMe(string.Format("Quaternion: {0},{1},{2},{3}", rotation.x, rotation.y, rotation.z, rotation.w));
+
+            // Before you move the object, remove any world anchor you may have on it.
+            PotatoManager.Instance.RemoveWorldAnchor();
+            PotatoManager.Instance.gameObject.transform.localPosition = vector;
+            PotatoManager.Instance.gameObject.transform.localRotation = rotation;
+            PotatoManager.Placing = false;
+            PotatoManager.HasPotato = true;
+            PotatoManager.Instance.SetWorldAnchor();
+        }
+
+        PotatoManager.Instance.UpdatePotatoColor();
+    }
+
+    private void SessionUsersTracker_UserLeft(User obj)
+    {
+        LogMe("SessionUsersTracker_UserLeft");
+        LogMe(string.Format("\tUser {0} left (Count Now: {1})", obj.GetName(), SharingStage.Instance.SessionUsersTracker.CurrentUsers.Count));
+    }
+
+    private void SessionUsersTracker_UserJoined(User obj)
+    {
+        LogMe("SessionUsersTracker_UserJoined");
+        LogMe(string.Format("\tUser {0} joined (Count Now: {1})", obj.GetName(), SharingStage.Instance.SessionUsersTracker.CurrentUsers.Count));
+    }
+
+    private void SessionsTracker_UserLeft(Session arg1, User arg2)
+    {
+        LogMe("SessionsTracker_UserLeft");
+        LogMe(string.Format("\tUser {0} left Session {1} (Count Now: {2})", arg2.GetName(), arg1.GetName(), arg1.GetUserCount()));
+        UpdateUsersInfo(arg1);
+    }
+
+    private void SessionsTracker_UserJoined(Session arg1, User arg2)
+    {
+        LogMe("SessionsTracker_UserJoined");
+        LogMe(string.Format("\tUser {0} joined Session {1} (Count Now: {2})", arg2.GetName(), arg1.GetName(), arg1.GetUserCount()));
+        UpdateUsersInfo(arg1);
+    }
+
+    private void SessionsTracker_CurrentUserJoined(Session obj)
+    {
+        LogMe("SessionsTracker_CurrentUserJoined");
+        LogMe(string.Format("\tYou have joined Session {0} (Count Now: {1})", obj.GetName(), obj.GetUserCount()));
+        UpdateUsersInfo(obj);
+    }
+
+    private void SessionsTracker_CurrentUserLeft(Session obj)
+    {
+        LogMe("SessionsTracker_CurrentUserLeft");
+        LogMe(string.Format("\tYou have left Session {0} (Count Now: {1})", obj.GetName(), obj.GetUserCount()));
+    }
+
+    private void SessionsTracker_UserChanged(Session arg1, User arg2)
+    {
+        LogMe("SessionsTracker_UserChanged");
+    }
+
+    private void SessionsTracker_SessionCreated(bool arg1, string arg2)
+    {
+        LogMe("SessionsTracker_SessionCreated");
+    }
+
+    private void SessionsTracker_SessionClosed(Session obj)
+    {
+        LogMe("SessionsTracker_SessionClosed");
+    }
+
+    private void SessionsTracker_SessionAdded(Session obj)
+    {
+        LogMe("SessionsTracker_SessionAdded");
+    }
+
+    private void SessionsTracker_ServerDisconnected()
+    {
+        LogMe("SessionsTracker_ServerDisconnected");
+    }
+
+    private void SessionsTracker_ServerConnected()
+    {
+        LogMe("SessionsTracker_ServerConnected");
+    }
+
+    private void Instance_SharingManagerConnected(object sender, EventArgs e)
+    {
+        LogMe("Instance_SharingManagerConnected");
+    }
+
+    private void LogMe(string message)
+    {
+        if (textMesh != null)
+        {
+            textMesh.text += string.Format("{0}\n", message);
+        }
+    }
+
+    private void UpdateUsersInfo(Session session)
+    {
+        // When you join or leave, check to see if you are the first or only user.
+        if (session.GetUserCount() == 1)
+        {
+            // You are the first, so you own the potato.
+            FirstUser = true;
+        }
+
+        PotatoManager.Instance.PotatoInitialize();
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        if (this.connection != null)
+        {
+            foreach (var index in Enum.GetValues(typeof(UserMessageId)))
+            {
+                this.connection.RemoveListener((byte)index, this.adapter);
+            }
+
+            this.adapter.MessageReceivedCallback -= Adapter_MessageReceivedCallback;
+        }
+    }
+
+    public void SendNetworkUserMessage(UserMessageId messageId, Transform objectTransform = null)
+    {
+        LogMe(string.Format("Sending Message: {0}", messageId));
+        if (this.connection != null)
+        {
+            var networkMessage = this.connection.CreateMessage((byte)messageId);
+
+            // Write the message id first
+            networkMessage.Write((byte)messageId);
+
+            // Now write something else, like the user's id
+            networkMessage.Write(this.localUserId);
+
+            if (objectTransform != null)
+            {
+                // Now send the transform info (Vector3 + Quaternion)
+                networkMessage.Write(objectTransform.localPosition.x);
+                networkMessage.Write(objectTransform.localPosition.y);
+                networkMessage.Write(objectTransform.localPosition.z);
+                networkMessage.Write(objectTransform.localRotation.x);
+                networkMessage.Write(objectTransform.localRotation.y);
+                networkMessage.Write(objectTransform.localRotation.z);
+                networkMessage.Write(objectTransform.localRotation.w);
+            }
+
+            // Broadcast this message to everyone on the network immediately
+            this.connection.Broadcast(networkMessage, MessagePriority.Immediate, MessageReliability.ReliableOrdered, MessageChannel.UserMessageChannelStart);
+        }
+    }
+}
